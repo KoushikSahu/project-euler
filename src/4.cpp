78801dab188f81e29d09d594f93d33ff4ab38aaa
Koushik Sahu <koushiksahu68@gmail.com>
2021-07-25 16:40:33 +0530

    fix 4.cpp

--- src/4.cpp
+++ src/4.cpp
@@ -1,13 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 bool palindrome_check(string a,int i,int j){
     if(a[i]==a[j] && i<=j){
         palindrome_check(a,i+1,j-1);
     }else{
-        return 0;}
+        return 0;
+    }
     return 1; 
 }
+
 int main()
 {
-    printf(palindrome_check("axyza",0,4));
+    cout<<palindrome_check("axyza",0,4)<<"\n";
 }
